dc5e352b6ad4d122c462899f29726d226d896569
Neagu Rares <neagu1000@gmail.com>
2016-04-01 12:49:37 +0300

    Migrated generated code into own folder (to decouple custom code). Updated migration to generate all models now.

--- Classes/Models/ModelEmployee.php
+++ Classes/Models/ModelEmployee.php
@@ -7,103 +7,12 @@
 
 namespace Models;
 
-use SmartModel;
-
 /**
  * Model Employee.
  * @package Models
  */
-class ModelEmployee extends SmartModel implements \ISessionable
+class ModelEmployee extends Generated\ModelEmployee implements \ISessionable
 {
-    /**
-     * @var int $employeeID
-     */
-    public $employeeID;
-
-    /**
-     * @var int|null $roleID
-     */
-    public $roleID;
-
-    /**
-     * @var string $account
-     */
-    public $account;
-
-    /**
-     * @var string $password
-     */
-    public $password;
-
-    /**
-     * @var int|null $managerID
-     */
-    public $managerID;
-
-    /**
-     * @var int|null $departmentID
-     */
-    public $departmentID;
-
-    /**
-     * @var string $firstName
-     */
-    public $firstName;
-
-    /**
-     * @var string|null $middleInitial
-     */
-    public $middleInitial;
-
-    /**
-     * @var string $lastName
-     */
-    public $lastName;
-
-    /**
-     * @var string $title
-     */
-    public $title;
-
-    /**
-     * @var string $cnp
-     */
-    public $cnp;
-
-    /**
-     * @var double $salary
-     */
-    public $salary;
-
-    /**
-     * @var double $priorSalary
-     */
-    public $priorSalary;
-
-    /**
-     * @var string $hireDate
-     */
-    public $hireDate;
-
-    /**
-     * @var string|null $terminationDate
-     */
-    public $terminationDate;
-
-    /**
-     * @var int $administrator
-     */
-    public $administrator;
-
-
-    /**
-     * ModelEmployee constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct($this, "Employees");
-    }
-
     /**
      * Reads the model from session.
      * @return false|ModelEmployee
